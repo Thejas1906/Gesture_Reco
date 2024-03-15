7382ae897869d270d55a2f4725650ba24a8643b8
Thejas1906 <thejasanbu5@gmail.com>
2024-03-15 21:10:15 +0530

    All work done

--- README.md
+++ README.md
@@ -1,1 +1,135 @@
-# Gesture_Reco
+<<<<<<< HEAD
+# Guesture_Reco
+Game developed by gesture recoginiton using computer vision.
+
+This Python program allows you to control the Hill Climb Racing and Dino Game using gesture recognition. It utilizes the OpenCV and Mediapipe libraries to capture and interpret hand gestures, enabling an interactive and hands-free gaming experience.
+
+Prerequisites:
+
+Before running the program, ensure you have the following dependencies installed:
+
+1.) Python 3.x
+2.) OpenCV
+3.) Mediapipe
+
+You can install the required libraries using the following command:
+
+--> pip install opencv-python mediapipe
+
+Navigate to the project directory:
+
+--> cd gesture-control-gaming
+
+Run the program:
+
+--> main.py
+
+1.) Follow the on-screen instructions to set up the hand gesture recognition. Adjust the camera angle and position for optimal performance.
+
+2.) Launch the Hill Climb Racing or Dino Game.
+
+3.) Use specific hand gestures to control the game. Refer to the gesture guide below.
+
+~~~~~GESTURE GUIDE~~~~~
+
+Hill Climb Racing Controls:
+
+NOTE: Choose the hand you want to use depending upon comfort and controls.
+
+Accelerate: Open your hand fully.(Left Hand)
+Brake/Reverse: Close your hand into a fist.(Left hand)
+
+Accelerate: Close Thumb and keep other 4 fingers open.(Right Hand)
+Brake/Reverse: Close all the 4 fingers and keep thumb open.(Right Hand)
+
+
+Dino Game Controls:
+
+Jump: Raise your hand up.
+Duck: Lower your hand down.
+
+Customization:
+
+Feel free to customize the gestures and controls according to your preference by modifying the main.py file. Adjust the gesture recognition parameters, mapping of gestures to game controls, and other settings as needed.
+
+Troubleshooting:
+
+If you encounter issues with hand gesture recognition or game control, consider adjusting the camera settings, gesture recognition thresholds, or experimenting with different gestures.
+
+Credits:
+
+This program was developed using Python, OpenCV, and Mediapipe. Special thanks to the contributors of these open-source libraries.
+
+
+
+
+
+
+=======
+# Guesture_Reco
+Game developed by gesture recoginiton using computer vision.
+
+This Python program allows you to control the Hill Climb Racing and Dino Game using gesture recognition. It utilizes the OpenCV and Mediapipe libraries to capture and interpret hand gestures, enabling an interactive and hands-free gaming experience.
+
+Prerequisites:
+
+Before running the program, ensure you have the following dependencies installed:
+
+1.) Python 3.x
+2.) OpenCV
+3.) Mediapipe
+
+You can install the required libraries using the following command:
+
+--> pip install opencv-python mediapipe
+
+Navigate to the project directory:
+
+--> cd gesture-control-gaming
+
+Run the program:
+
+--> main.py
+
+1.) Follow the on-screen instructions to set up the hand gesture recognition. Adjust the camera angle and position for optimal performance.
+
+2.) Launch the Hill Climb Racing or Dino Game.
+
+3.) Use specific hand gestures to control the game. Refer to the gesture guide below.
+
+~~~~~GESTURE GUIDE~~~~~
+
+Hill Climb Racing Controls:
+
+NOTE: Choose the hand you want to use depending upon comfort and controls.
+
+Accelerate: Open your hand fully.(Left Hand)
+Brake/Reverse: Close your hand into a fist.(Left hand)
+
+Accelerate: Close Thumb and keep other 4 fingers open.(Right Hand)
+Brake/Reverse: Close all the 4 fingers and keep thumb open.(Right Hand)
+
+
+Dino Game Controls:
+
+Jump: Raise your hand up.
+Duck: Lower your hand down.
+
+Customization:
+
+Feel free to customize the gestures and controls according to your preference by modifying the main.py file. Adjust the gesture recognition parameters, mapping of gestures to game controls, and other settings as needed.
+
+Troubleshooting:
+
+If you encounter issues with hand gesture recognition or game control, consider adjusting the camera settings, gesture recognition thresholds, or experimenting with different gestures.
+
+Credits:
+
+This program was developed using Python, OpenCV, and Mediapipe. Special thanks to the contributors of these open-source libraries.
+
+
+
+
+
+
+>>>>>>> 359639cbc1aeae22a997109c90b348a55e29fc48
---
+++ main.py
@@ -0,0 +1,314 @@
+<<<<<<< HEAD
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from directkeys import PressKey, ReleaseKey
+from directkeys import up_pressed,down_pressed,right_pressed,left_pressed,a_pressed,d_pressed
+import time
+
+
+from flask import Flask, render_template, request, url_for
+from main import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/run_program')
+def run_program():
+    gesture_reco()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def gesture_reco():
+    detector=HandDetector(detectionCon=0.8, maxHands=2)
+
+
+    #Hill Climb Racing 
+    brake_key_pressed = a_pressed
+    accelerator_key_pressed = d_pressed
+
+    #Subway Surfers
+    left_key_pressed = left_pressed
+    right_key_pressed = right_pressed
+    up_key_pressed = up_pressed
+    down_key_pressed = down_pressed
+
+
+    time.sleep(2.0)
+    current_key_pressed = set()
+
+    video=cv2.VideoCapture(0)
+    cv2.namedWindow('Gesture Recognition', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Gesture Recognition', 460, 460)
+
+
+    while True:
+        ret,image=video.read()
+        keyPressed = False
+        spacePressed=False
+        key_count=0
+        key_pressed=0   
+        hands,img=detector.findHands(image)
+        #image = cv2.flip(img,1)
+        cv2.rectangle(image, (470, 640), (0, 750),(50, 50, 255), -2)
+        cv2.rectangle(image, (850, 640), (1300, 750),(50, 50, 255), -2)
+        if hands:
+            lmList=hands[0]
+            fingerUp=detector.fingersUp(lmList)
+
+            print(fingerUp)
+
+            # Subway Surfers - Jump 
+            if fingerUp==[0,1,0,0,0]:
+                cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Jump', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(up_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(up_key_pressed)
+                key_pressed=up_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Subway Surfers - Roll
+            if fingerUp==[1,1,1,1,1]:
+                cv2.putText(image, 'Finger Count: 0', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Roll', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(down_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(down_key_pressed)
+                key_pressed=down_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Subway Surfers - Left    
+            if fingerUp==[1,0,0,0,0]:
+                cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Left', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(left_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(left_key_pressed)
+                key_pressed=left_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Subway Surfers - Right
+            if fingerUp==[0,0,0,0,1]:
+                cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Right', (990,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(right_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(right_key_pressed)
+                key_pressed=right_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Hill Climb Racing - Gas
+            if fingerUp==[0,1,1,1,1]:
+                cv2.putText(image, 'Finger Count: 4', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Gas', (1020,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(accelerator_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(accelerator_key_pressed)
+                key_pressed=accelerator_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Hill Climb Racing - Brake    
+            if fingerUp==[0,1,1,0,0]:
+                cv2.putText(image, 'Finger Count: 2', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Brake', (980,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                PressKey(brake_key_pressed)
+                spacePressed=True
+                current_key_pressed.add(brake_key_pressed)
+                key_pressed=brake_key_pressed
+                keyPressed = True
+                key_count=key_count+1
+
+            #Virtual Touch
+            if fingerUp==[0,1,0,0,1]:
+                cv2.putText(image, 'Finger Count: 5', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Stand-By', (950,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+
+                
+            # Stand-By
+            if fingerUp==[0,0,0,0,0]:
+                cv2.putText(image, 'Finger Count: 5', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+                cv2.putText(image, 'Stand-By', (950,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+
+
+            if not keyPressed and len(current_key_pressed) != 0:
+                for key in current_key_pressed:
+                    ReleaseKey(key)
+                current_key_pressed = set()
+            elif key_count==1 and len(current_key_pressed)==2:    
+                for key in current_key_pressed:             
+                    if key_pressed!=key:
+                        ReleaseKey(key)
+                current_key_pressed = set()
+                for key in current_key_pressed:
+                    ReleaseKey(key)
+                current_key_pressed = set()
+
+
+        cv2.imshow("Gesture Recognition",image)
+        k=cv2.waitKey(1)
+        if k==ord('x'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+
+=======
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from directkeys import PressKey, ReleaseKey
+from directkeys import up_pressed,down_pressed,right_pressed,left_pressed,a_pressed,d_pressed
+import time
+
+detector=HandDetector(detectionCon=0.8, maxHands=2)
+
+#space_key_pressed=space_pressed
+
+#Hill Climb Racing 
+brake_key_pressed = a_pressed
+accelerator_key_pressed = d_pressed
+
+#Subway Surfers
+left_key_pressed = left_pressed
+right_key_pressed = right_pressed
+up_key_pressed = up_pressed
+down_key_pressed = down_pressed
+
+
+time.sleep(2.0)
+current_key_pressed = set()
+
+video=cv2.VideoCapture(0)
+cv2.namedWindow('Gesture Recognition', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Gesture Recognition', 460, 460)
+
+
+while True:
+    ret,image=video.read()
+    keyPressed = False
+    spacePressed=False
+    key_count=0
+    key_pressed=0   
+    hands,img=detector.findHands(image)
+    #image = cv2.flip(img,1)
+    cv2.rectangle(image, (470, 640), (0, 750),(50, 50, 255), -2)
+    cv2.rectangle(image, (850, 640), (1300, 750),(50, 50, 255), -2)
+    if hands:
+        lmList=hands[0]
+        fingerUp=detector.fingersUp(lmList)
+
+        print(fingerUp)
+
+        # Subway Surfers - Jump 
+        if fingerUp==[0,1,0,0,0]:
+            cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Jump', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(up_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(up_key_pressed)
+            key_pressed=up_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Subway Surfers - Roll
+        if fingerUp==[1,1,1,1,1]:
+            cv2.putText(image, 'Finger Count: 0', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Roll', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(down_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(down_key_pressed)
+            key_pressed=down_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Subway Surfers - Left    
+        if fingerUp==[1,0,0,0,0]:
+            cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Left', (1000,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(left_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(left_key_pressed)
+            key_pressed=left_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Subway Surfers - Right
+        if fingerUp==[0,0,0,0,1]:
+            cv2.putText(image, 'Finger Count: 1', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Right', (990,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(right_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(right_key_pressed)
+            key_pressed=right_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Hill Climb Racing - Gas
+        if fingerUp==[0,1,1,1,1]:
+            cv2.putText(image, 'Finger Count: 4', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Gas', (1020,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(accelerator_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(accelerator_key_pressed)
+            key_pressed=accelerator_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Hill Climb Racing - Brake    
+        if fingerUp==[0,1,1,0,0]:
+            cv2.putText(image, 'Finger Count: 2', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Brake', (980,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            PressKey(brake_key_pressed)
+            spacePressed=True
+            current_key_pressed.add(brake_key_pressed)
+            key_pressed=brake_key_pressed
+            keyPressed = True
+            key_count=key_count+1
+
+        #Virtual Touch
+        if fingerUp==[0,1,0,0,1]:
+            cv2.putText(image, 'Finger Count: 5', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Stand-By', (950,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+
+            
+        # Stand-By
+        if fingerUp==[0,0,0,0,0]:
+            cv2.putText(image, 'Finger Count: 5', (30,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'Stand-By', (950,695), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+
+
+        if not keyPressed and len(current_key_pressed) != 0:
+            for key in current_key_pressed:
+                ReleaseKey(key)
+            current_key_pressed = set()
+        elif key_count==1 and len(current_key_pressed)==2:    
+            for key in current_key_pressed:             
+                if key_pressed!=key:
+                    ReleaseKey(key)
+            current_key_pressed = set()
+            for key in current_key_pressed:
+                ReleaseKey(key)
+            current_key_pressed = set()
+
+
+    cv2.imshow("Gesture Recognition",image)
+    k=cv2.waitKey(1)
+    if k==ord('x'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+>>>>>>> 359639cbc1aeae22a997109c90b348a55e29fc48
